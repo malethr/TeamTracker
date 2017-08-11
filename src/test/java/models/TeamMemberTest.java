8cf78bebdffa9364c85e3ee0b7c790ce64c5fc68
Maria Thomas <malethr@gmail.com>
2017-08-11 10:59:10 -0700

    add TeamMember class content and its test

--- src/test/java/models/TeamMemberTest.java
+++ src/test/java/models/TeamMemberTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by mariathomas on 8/11/17.
@@ -28,6 +29,49 @@ public class TeamMemberTest {
 
     @After
     public void tearDown() throws Exception {
+        TeamMember.clearAllTeamMembers();
+    }
+
+    public TeamMember setUpNewTeamMember(){
+        return new TeamMember("Jane Doe");
+    }
+
+    @Test
+    public void AllTeamMembersCorrectlyReturned_2() {
+        TeamMember teamMember = setUpNewTeamMember();
+        TeamMember otherTeam = new TeamMember ("John Smith");
+        assertEquals(2, TeamMember.getAllTeamMembers().size());
+    }
+
+    @Test
+    public void AllTeamMembersContainsAllTeamMemberNames_true() {
+        TeamMember teamMember = setUpNewTeamMember();
+        TeamMember otherTeamMember = new TeamMember ("John Smith");
+        assertTrue(TeamMember.getAllTeamMembers().contains(teamMember));
+        assertTrue(TeamMember.getAllTeamMembers().contains(otherTeamMember));
+    }
+
+    @Test
+    public void clearAllTeamMembers_checkIfClearsData_0() {
+        TeamMember teamMember = setUpNewTeamMember();
+        TeamMember otherTeamMember = new TeamMember ("John Smith");
+        TeamMember.clearAllTeamMembers();
+        assertEquals(0,TeamMember.getAllTeamMembers().size());
+    }
+
+    @Test
+    public void getMemberId_teamMemberInstantiateWithAnID_1() {
+        TeamMember teamMember = setUpNewTeamMember();
+        TeamMember otherTeamMember = new TeamMember ("John Smith");
+        assertEquals(1, teamMember.getMemberId());
+        assertEquals(2, otherTeamMember.getMemberId());
+    }
+
+    @Test
+    public void findByMemberId_getTeamMemberInstance_otherTeamMember(){
+        TeamMember teamMember = setUpNewTeamMember();
+        Team otherTeam = new Team ("John Smith");
+        assertEquals(otherTeam, Team.findById(otherTeam.getId()));
     }
 
 }
