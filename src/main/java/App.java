ad3afe643e22b822577eef376cedd7a9fa381bdd
Maria Thomas <malethr@gmail.com>
2017-08-11 12:54:56 -0700

    add branching to index.hbs and modify the first route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,26 @@
+import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 /**
  * Created by mariathomas on 8/11/17.
  */
+
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
         get("/", (req,res)->{
            Map<String, Object> model = new HashMap<>();
+           ArrayList<Team> teams = Team.getAll();
+           model.put("teams",teams);
            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
     }
