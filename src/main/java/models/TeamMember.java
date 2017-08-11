c258e2990c9e3b13f67fd6963ad5e1a1297006c3
Maria Thomas <malethr@gmail.com>
2017-08-11 10:21:20 -0700

    add initial content to TeamMember class

--- src/main/java/models/TeamMember.java
+++ src/main/java/models/TeamMember.java
@@ -4,4 +4,14 @@ package models;
  * Created by mariathomas on 8/11/17.
  */
 public class TeamMember {
+
+    private String teamMemberName;
+
+    public TeamMember (String teamMemberName){
+        this.teamMemberName = teamMemberName;
+    }
+
+    public String getTeamMemberName() {
+        return teamMemberName;
+    }
 }
