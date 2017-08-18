41fc906935e232b37348a8ad648c484fd21d0231
Maria Thomas <malethr@gmail.com>
2017-08-18 10:07:38 -0700

    create initial sql dao and test files

---
+++ src/main/java/dao/Sql2oTeamDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by mariathomas on 8/18/17.
+ */
+public class Sql2oTeamDao {
+}
---
+++ src/test/java/dao/Sql2oTeamDaoTest.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by mariathomas on 8/18/17.
+ */
+public class Sql2oTeamDaoTest {
+
+}
